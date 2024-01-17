8553ee1e808bc6fef6380b88f5b594824a452322
lucasbernardo842 <lucasbernardo842@gmail.com>
2024-01-16 20:15:34 -0500

    att

--- JSAlgoritmosDeOrdenacao/aula2/selectionSort.js
+++ JSAlgoritmosDeOrdenacao/aula2/selectionSort.js
@@ -2,20 +2,20 @@ const livros = require("../aula1/listalivros");
 const menorValor = require("../aula1/menorValor");
 const troca = require('../aula3/troca')
 
-// for (let atual = 0; atual < livros.length - 1; atual++){
-//     let menor = menorValor(livros, atual);
+for (let atual = 0; atual < livros.length - 1; atual++){
+    let menor = menorValor(livros, atual);
 
-//     let livroAtual = livros[atual];
-//     let livroMenorPreco = livros[menor];
+    let livroAtual = livros[atual];
+    let livroMenorPreco = livros[menor];
 
-//     livros[atual] = livroMenorPreco;
-//     livros[menor] = livroAtual;
-// };
+    livros[atual] = livroMenorPreco;
+    livros[menor] = livroAtual;
+};
 
 // console.log(livros);
-livros.forEach((_, indice) => {
-    let menor = menorValor(livros, indice);
-    troca(livros, indice);
-});
+// livros.forEach((_, indice) => {
+//     let menor = menorValor(livros, indice);
+//     troca(livros, indice);
+// });
 
 console.log(livros)
